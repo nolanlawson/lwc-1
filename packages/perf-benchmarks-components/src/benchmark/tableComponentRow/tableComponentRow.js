b23fc4c237ddab6d784e523256432c2a5b1d3001
Nolan Lawson <nlawson@salesforce.com>
2021-10-29 13:26:57 -0700

    perf: add @track to perf tests

--- packages/perf-benchmarks-components/src/benchmark/tableComponentRow/tableComponentRow.js
+++ packages/perf-benchmarks-components/src/benchmark/tableComponentRow/tableComponentRow.js
@@ -4,10 +4,23 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class TableComponentRow extends LightningElement {
-    @api row;
+    // Does nothing, just used to test @track
+    @track rowCopy = {};
+
+    _row;
+
+    @api
+    set row(row) {
+        Object.assign(this.rowCopy, row); // copy data over
+        this._row = row;
+    }
+
+    get row() {
+        return this._row;
+    }
 
     handleSelect() {
         this.dispatchEvent(new CustomEvent('select'));
