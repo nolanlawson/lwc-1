0b486d58cff235ff59772694dac49e0240269ce4
Nolan Lawson <nlawson@salesforce.com>
2022-07-29 14:22:30 -0700

    test: fix test

--- packages/@lwc/engine-server/src/__tests__/fixtures/parent-child-read-only-prop/modules/x/child/child.js
+++ packages/@lwc/engine-server/src/__tests__/fixtures/parent-child-read-only-prop/modules/x/child/child.js
@@ -8,28 +8,26 @@ export default class extends LightningElement {
   result
 
   connectedCallback() {
-
-    let result = []
+    const results = []
     
     try {
       this.array.push('bar')
     } catch (err) {
-      result.push('array: error hit during mutation')
+      results.push('array: error hit during mutation')
     }
 
     try {
       this.object.foo = 'baz'
     } catch (err) {
-      result.push('object: error hit during mutation')
+      results.push('object: error hit during mutation')
     }
     
     try {
       this.deep.foo[0].quux = 'quux'
-      result += '${name}: No error'
     } catch (err) {
-      result.push('deep: error hit during mutation')
+      results.push('deep: error hit during mutation')
     }
 
-    this.result = '\n' + result.join('\n')
+    this.result = '\n' + results.join('\n')
   }
 }
