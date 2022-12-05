1484dc5fb5d51b42c08d5d996855aed57d7acc2a
Nolan Lawson <nlawson@salesforce.com>
2022-12-05 12:29:48 -0800

    feat: reporting control

---
+++ packages/@lwc/engine-core/src/framework/reporting.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+import { noop } from '@lwc/shared';
+import { VM } from './vm';
+
+type ReportingDispatcher = (reportId: any, tagName?: string, vmIndex?: number) => void;
+
+/** Indicates if reporting is enabled or not */
+let enabled = false;
+
+/** The currently assigned reporting dispatcher. */
+let currentDispatcher: ReportingDispatcher = noop;
+
+export const reportingControl = {
+    /**
+     * Attach a new reporting control (aka dispatcher).
+     *
+     * @param dispatcher - reporting control
+     */
+    attachReportingControl(dispatcher: ReportingDispatcher): void {
+        currentDispatcher = dispatcher;
+
+        enabled = true;
+    },
+
+    /**
+     * Detach the current reporting control (aka dispatcher).
+     */
+    detachReportingControl(): void {
+        currentDispatcher = noop;
+        enabled = false;
+    },
+};
+
+/**
+ * True if a reporting dispatcher is currently attached.
+ */
+export function isReportingEnabled(): boolean {
+    return enabled;
+}
+
+/**
+ * Report to the current dispatcher, if there is one.
+ * @param reportId
+ * @param vm
+ */
+export function report(reportId: any, vm?: VM) {
+    if (enabled) {
+        currentDispatcher(reportId, vm?.tagName, vm?.idx);
+    }
+}
