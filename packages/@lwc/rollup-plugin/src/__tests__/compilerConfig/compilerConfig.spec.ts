4710301286755eb971e13bb7e28d832528ebcfb5
Nolan Lawson <nlawson@salesforce.com>
2023-02-17 10:10:58 -0800

    test: rollup test

--- packages/@lwc/rollup-plugin/src/__tests__/compilerConfig/compilerConfig.spec.ts
+++ packages/@lwc/rollup-plugin/src/__tests__/compilerConfig/compilerConfig.spec.ts
@@ -6,6 +6,7 @@
  */
 import path from 'path';
 import { rollup } from 'rollup';
+import { APIVersion } from '@lwc/shared';
 
 import lwc from '../../index';
 
@@ -73,5 +74,24 @@ describe('templateConfig', () => {
         // This function takes no arguments, corresponding to the optimization used
         // for `disableSyntheticShadowSupport: true` by serialize.ts in @lwc/style-compiler
         expect(output[0].code).toContain('function stylesheet()');
+    });
+});
+
+describe('API versioning', () => {
+    it('passes the apiVersion on to the compiled JS component', async () => {
+        const bundle = await rollup({
+            input: path.resolve(__dirname, 'fixtures/test/test.js'),
+            plugins: [
+                lwc({
+                    apiVersion: APIVersion.V58,
+                }),
+            ],
+        });
+
+        const { output } = await bundle.generate({
+            format: 'esm',
+        });
+
+        expect(output[0].code).toContain('v: 58');
     });
 });
