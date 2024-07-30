70d6a474420cded069ef3e6d77cbc04c0c14e38a
Nolan Lawson <nlawson@salesforce.com>
2024-07-30 13:54:35 -0700

    chore: try to upgrade tachometer

--- packages/@lwc/perf-benchmarks/scripts/fix-deps.sh
+++ packages/@lwc/perf-benchmarks/scripts/fix-deps.sh
@@ -9,6 +9,10 @@
 # This is bizarre, but it seems to be the only way to get Tachometer to recognize
 # our local deps, e.g. when running in `this-patch` mode (not `tip-of-tree` mode).
 # See: https://github.com/Polymer/tachometer/issues/215
+#
+# Additionally, create node_modules symlinks alongside every *.tachometer.json file. This fixes an
+# additional issue where Tachometer can only recognize package imports relative to config files.
+# See: https://github.com/google/tachometer/issues/244
 
 set -e
 
@@ -17,5 +21,12 @@ mkdir -p ./node_modules/@lwc
 for pkg in @lwc/engine-dom @lwc/engine-server @lwc/synthetic-shadow @lwc/perf-benchmarks-components; do
   if [ ! -L "./node_modules/$pkg" ]; then
     ln -s "../../../../../packages/$pkg" "./node_modules/$pkg"
+  fi
+done
+
+for json in $(find dist -name '*.tachometer.json'); do
+  parent_dir=$(dirname "$json")
+  if [ ! -L "${parent_dir}/node_modules" ]; then
+    ln -sr ./node_modules "${parent_dir}/node_modules"
   fi
 done
