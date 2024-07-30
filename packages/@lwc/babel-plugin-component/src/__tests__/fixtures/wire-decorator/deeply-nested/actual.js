f978a4a756ea39d42ae4a79f69b6f9610b62aac5
Nolan Lawson <nlawson@salesforce.com>
2024-07-30 14:04:16 -0700

    test: nested vs not

---
+++ packages/@lwc/babel-plugin-component/src/__tests__/fixtures/wire-decorator/deeply-nested/actual.js
@@ -0,0 +1,6 @@
+import { wire, LightningElement } from "lwc";
+import { getFoo } from "data-service";
+export default class Test extends LightningElement {
+  @wire(getFoo, { nested: { object: '$prop' } })
+  wiredProp;
+}
---
+++ packages/@lwc/babel-plugin-component/src/__tests__/fixtures/wire-decorator/deeply-nested/expected.js
@@ -0,0 +1,28 @@
+import { registerDecorators as _registerDecorators, LightningElement, registerComponent as _registerComponent } from "lwc";
+import _tmpl from "./test.html";
+import { getFoo } from "data-service";
+class Test extends LightningElement {
+  wiredProp;
+  /*LWC compiler vX.X.X*/
+}
+_registerDecorators(Test, {
+  wire: {
+    wiredProp: {
+      adapter: getFoo,
+      dynamic: [],
+      config: function ($cmp) {
+        return {
+          nested: {
+            object: '$prop'
+          }
+        };
+      }
+    }
+  }
+});
+const __lwc_component_class_internal = _registerComponent(Test, {
+  tmpl: _tmpl,
+  sel: "lwc-test",
+  apiVersion: 9999999
+});
+export default __lwc_component_class_internal;
